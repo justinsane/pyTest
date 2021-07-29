39d753f26b700f0196926a227ac4ff1ba620d2f0
justlevy <justin@justlevy.com>
2021-07-28 20:36:11 -0700

    ex2 solution

--- ex2_RE.py
+++ ex2_RE.py
@@ -1,14 +1,30 @@
 # Exercise 2: Write a program to look for lines of the form:
 
 # New Revision: 39772
+# Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers and print out the average as an integer.
+
+# Enter file:mbox.txt
+# 38549
+
+# Enter file:mbox-short.txt
+# 39756
 
 import re
 new_list = list()
+count = 0
+sum = 0
 
-fhand = open('mbox.txt')
+filename = input("Enter file:  ")
+fhand = open(filename)
 for line in fhand:
     line = line.strip()
-    x = re.findall('(New Revision: \d+)', line)
+    x = re.findall('New Revision: (\d+)', line)
+
     if len(x) > 0:
         new_list.append(x)
-print(new_list[0])
+        count += 1
+        for integer in x:
+            sum = sum + int(integer)
+average = int(sum/count)
+print(average)
+        
