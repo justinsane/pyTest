f3d1d8e87e16950fe8953093772a432421789eb3
justlevy <justin@justlevy.com>
2021-07-28 20:16:16 -0700

    exercise 2 - complete

--- ex2_RE.py
+++ ex2_RE.py
@@ -1,3 +1,14 @@
 # Exercise 2: Write a program to look for lines of the form:
 
 # New Revision: 39772
+
+import re
+new_list = list()
+
+fhand = open('mbox.txt')
+for line in fhand:
+    line = line.strip()
+    x = re.findall('(New Revision: \d+)', line)
+    if len(x) > 0:
+        new_list.append(x)
+print(new_list[0])
