888b741e3173b047d5752a943f7e9a830aa5b969
justlevy <justin@justlevy.com>
2021-07-31 13:57:30 -0700

    working on exception handling

---
+++ ex1_Networking.py
@@ -0,0 +1,35 @@
+'''
+Exercise 1: Change the socket program socket1.py to prompt the user for the URL so it can read any web page. 
+You can use split('/') to break the URL into its component parts so you can extract the host name for the socket 
+connect call. Add error checking using try and except to handle the condition where the user enters an improperly 
+formatted or non-existent URL.
+'''
+
+# WORK ON THIS:
+# Add error checking using try and except to handle the condition where the user enters an improperly formatted or non-existent URL.
+
+import socket
+import re
+
+while True:
+    try:
+        my_web = input('Enter URL:  ')
+        re_web = re.findall('[http://]*[www.]*(\S+.com)', my_web)
+        break
+    except:
+        print('Enter a valid URL.')
+        continue
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((re_web[0], 80))
+cmd = 'GET my_web HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
+
+# Code: http://www.py4e.com/code3/socket1.py
