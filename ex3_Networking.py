e10dd9e8336fb1e53ee90b57695f5ec96960c30f
justlevy <justin@justlevy.com>
2021-08-02 19:50:10 -0700

    solved exercise 3

--- ex3_Networking.py
+++ ex3_Networking.py
@@ -1,31 +1,28 @@
 '''
-Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL, (2) displaying up to 3000 characters, 
-and (3) counting the overall number of characters in the document. Don’t worry about the headers for this exercise, simply show the first 
-3000 characters of the document contents.
+Exercise 3: Use urllib to replicate the previous exercise of (1) retrieving the document from a URL, 
+(2) displaying up to 100 characters, and (3) counting the overall number of characters in the 
+document. Don’t worry about the headers for this exercise, simply show the first 100 characters of 
+the document contents.
 '''
+import requests
 
-import socket
-
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/mbox-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
-
+html = requests.get('http://data.pr4e.org/romeo.txt')
+num_words = int(input('Show how many characters: '))
 char_count = 0
 
-while True:
-    data = mysock.recv(1)
-    if len(data) < 1:
-        break
-    for char in data.strip():
-        char_count += 1
-    if char_count <= 2999:
-        print(data.decode(), end='')
-    else:
-        break
-print()
-print('Total Character Count is: ', char_count)
+for line in html:
+    data = len(line.decode().strip())
+    char_count += data
 
-mysock.close()
+print(html.content.decode().strip()[0:num_words])
+print('Total number of words: ', char_count)
+
+
+# for line in html:
+#     words = line.decode().strip()
+#     for word in words:
+#         char_count += 1
+# print(html.content.decode().strip()[0:num_words])
+# print('Total number of words: ', char_count)
 
 # Code: http://www.py4e.com/code3/socket1.py
