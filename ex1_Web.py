c34a2e730fa948f11e558bd9dd2dd58651740310
justlevy <justin@justlevy.com>
2021-08-06 07:02:18 -0700

    exercise 1 changes

--- ex1_Web.py
+++ ex1_Web.py
@@ -50,12 +50,13 @@ while True:
         print(data)
         continue
 
-    print(json.dumps(js, indent=4))
+    # print(json.dumps(js, indent=4))
 
-    lat = js['results'][0]['geometry']['location']['lat']
-    lng = js['results'][0]['geometry']['location']['lng']
-    print('lat', lat, 'lng', lng)
-    location = js['results'][0]['formatted_address']
-    print(location)
+    # lat = js['results'][0]['geometry']['location']['lat']
+    # lng = js['results'][0]['geometry']['location']['lng']
+    # print('lat', lat, 'lng', lng)
+    # location = js['results'][0]['formatted_address']
+    country = js['results'][0]['address_components'][3]['short_name']
+    print('Country Code: ', country)
 
 # Code: http://www.py4e.com/code3/geojson.py
